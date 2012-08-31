31538bfcd3c55805b0515d369d9ce195d1aef7df
Ivan Willig <iwillig@opengeo.org>
2012-08-31 08:37:59 -0400

    Nicer names for files. add the start of chapter 2.

---
+++ chapter2.scm
@@ -0,0 +1,23 @@
+#lang racket
+
+;; chapter 2, Building abstraction with data
+
+(define (make-rat n d) (conds n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+
+            
